c448f6c05e81266dbcfea30c5184817bad15dfd0
Jubstaaa <ilkerbalcilartr@gmail.com>
2022-09-13 19:28:20 +0300

    project finished

--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -7,14 +7,16 @@ const PrivateRoute = ({ isAuthenticated, component: Component, ...rest }) => (
   <Route
     {...rest}
     component={(props) =>
-      isAuthenticated === "CuAvKmPxuwen97wHRLJ35JJYppr2" ||
+      isAuthenticated === "CuAvKmPxuwen97wHRLJ35JJYppr2" &&
       "ve1cb2xmifb17pDJyZK7JSL5BXF3" ? (
         <div>
+          {console.log(isAuthenticated)}
           <Component {...props} />
         </div>
       ) : (
         <>
           <Redirect to="/" />
+          {console.log(isAuthenticated)}
           {alert(
             "Only Admins Can Edit Messages Please Contact github.com/Jubstaaa"
           )}
